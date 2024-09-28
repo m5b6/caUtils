1e29f19d438808286aaceb7ea31558a754fd02b5
m5b6 😽 <77901615+m5b6@users.noreply.github.com>
2024-09-28 12:39:25 -0300

    refactor doctor profile processing for improved concurrency and error logging, enhancing progress tracking with a progress bar

--- get_doctors/src/doctor_processor.rs
+++ get_doctors/src/doctor_processor.rs
@@ -1,42 +1,59 @@
-// doctor_processor.rs
 use crate::doctor_fetcher::fetch_doctor_profile;
 use crate::models::{DoctorOverview, DoctorProfile};
 use futures::{stream, StreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Client;
 
+const CONCURRENT_REQUESTS: usize = 10;
+
 pub async fn process_doctors(client: &Client, doctors: Vec<DoctorOverview>) -> Vec<DoctorProfile> {
-    let total_doctors = doctors.len() as u64;
+    let progress_bar = create_progress_bar(doctors.len());
 
-    let pb = ProgressBar::new(total_doctors);
+    let doctor_profiles = fetch_doctor_profiles(client, doctors, progress_bar.clone()).await;
+
+    progress_bar.finish_with_message("Processing complete");
+
+    doctor_profiles
+}
+
+fn create_progress_bar(total_doctors: usize) -> ProgressBar {
+    let pb = ProgressBar::new(total_doctors as u64);
     pb.set_style(ProgressStyle::default_bar());
+    pb
+}
 
-    let profiles = stream::iter(doctors)
-        .map(|doctor| {
-            let client = client.clone();
-            let pb = pb.clone();
-            let rut = doctor.rut;
-            async move {
-                match fetch_doctor_profile(&client, rut).await {
-                    Ok(profile) => {
-                        pb.inc(1);
-                        Some(profile)
-                    }
-                    Err(e) => {
-                        log::error!("The failing doctor is {:?}", doctor);
-                        log::error!("Error fetching profile for RUT {}: {}", rut, e);
-                        pb.inc(1);
-                        None
-                    }
-                }
-            }
-        })
-        .buffer_unordered(10)
+async fn fetch_doctor_profiles(
+    client: &Client,
+    doctors: Vec<DoctorOverview>,
+    progress_bar: ProgressBar,
+) -> Vec<DoctorProfile> {
+    stream::iter(doctors)
+        .map(|doctor| fetch_single_doctor_profile(client, doctor, progress_bar.clone()))
+        .buffer_unordered(CONCURRENT_REQUESTS)
         .filter_map(|result| async move { result })
         .collect()
-        .await;
+        .await
+}
 
-    pb.finish_with_message("Processing complete");
+async fn fetch_single_doctor_profile(
+    client: &Client,
+    doctor: DoctorOverview,
+    progress_bar: ProgressBar,
+) -> Option<DoctorProfile> {
+    match fetch_doctor_profile(client, doctor.rut).await {
+        Ok(profile) => {
+            progress_bar.inc(1);
+            Some(profile)
+        }
+        Err(e) => {
+            log_error(&doctor, &e);
+            progress_bar.inc(1);
+            None
+        }
+    }
+}
 
-    profiles
+fn log_error(doctor: &DoctorOverview, error: &Box<dyn std::error::Error>) {
+    log::error!("The failing doctor is {:?}", doctor);
+    log::error!("Error fetching profile for RUT {}: {}", doctor.rut, error);
 }
